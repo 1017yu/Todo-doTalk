4818ba297450aa2a7ed5034dccc5a7daf7951412
Kled <1017yu@daum.net>
2023-05-13 18:20:28 +0900

    feat: 저장된 TodoList title 출력 기능 추가

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,29 @@
-import { useRecoilValue } from "recoil";
-import { useSetRecoilState } from "recoil";
-import { todoListState } from "~/src/states/todoAtoms";
+import React, { useEffect } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import TodoItemCreator from "~/src/components/TodoItemCreator";
 import TodoItem from "~/src/components/TodoItem";
-import { fetchTodos } from "../lib";
+import { todoListState } from "~/src/states/todoAtoms";
+import { getTodo } from "~/src/lib";
 
 function TodoList() {
+  // `useSetRecoilState` todoListState recoil atom 상태 값 업데이트
   const setTodoList = useSetRecoilState(todoListState);
 
-  const initList = async () => {
-    const oldTodo = await fetchTodos();
-    setTodoList((state) => [...state, oldTodo]);
-  };
+  // `useRecoilState` todoListState recoil 상태 값 동적 관리
+  const [todoList] = useRecoilState(todoListState);
 
-  const todoList = useRecoilValue(todoListState);
+  // 컴포넌트가 마운트 될 때, getTodo를 호출하여 최초 렌더링 시
+  // GET으로 가져온 TodoList를 recoil 상태 값에 업데이트
+  useEffect(() => {
+    const fetchTodo = async () => {
+      // GET 요청
+      const data = await getTodo();
+      // setTodoList를 통해 GET 요청으로 반환된 data를 todoListState atom에 저장
+      setTodoList(data);
+    };
 
-  console.log(todoList);
+    fetchTodo();
+  }, []);
 
   return (
     <>
