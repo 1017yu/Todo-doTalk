1544f1bd113736184f5ea56cf0aa82b457e7f15e
Kled <1017yu@daum.net>
2023-05-14 00:15:31 +0900

    feat: Home 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import TodoItemCreator from "../components/todoItemCreator";
+import TodoList from "../components/TodoList";
 
 function Home() {
-  const [counter, setCounter] = useState(0);
-
   return (
-    <div>
-      <TodoItemCreator />
-      <h1>Hello {counter}</h1>
-      <button onClick={() => setCounter((prev) => (prev += 1))}>+</button>
-    </div>
+    <>
+      <TodoList />
+    </>
   );
 }
 
