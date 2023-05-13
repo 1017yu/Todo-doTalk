19ace5ca4f7d7674d5148c8d0213aeb847f9f27f
Kled <1017yu@daum.net>
2023-05-14 00:14:51 +0900

    docs: Fetch 함수명 수정

--- src/lib/index.js
+++ src/lib/index.js
@@ -3,25 +3,23 @@ const options = (method, data) => ({
   headers: {
     "content-type": "application/json",
     apikey: "KDT5_nREmPe9B",
-    username: "KDT5_YuHeeTae",
+    username: "KDT5__YuHeeTae",
   },
   body: JSON.stringify(data),
 });
 
-const url = "https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos";
+const URL = "https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos";
 
 // 항목 생성
 const createTodo = async (title) => {
-  const res = await fetch(`${url}`, options("POST", { title }));
+  const res = await fetch(URL, options("POST", { title }));
   const data = await res.json();
   return data;
 };
 
 // 목록 조회
-const fetchTodos = async () => {
-  const res = await fetch(url, {
-    method: "GET",
-  });
+const getTodo = async () => {
+  const res = await fetch(URL, options("GET"));
   const data = await res.json();
   return data;
 };
@@ -42,4 +40,4 @@ const deleteTodo = async (id) => {
   return data;
 };
 
-export { createTodo, fetchTodos, updateTodo, deleteTodo };
+export { createTodo, getTodo, updateTodo, deleteTodo };
