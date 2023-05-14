99f7e7d1c50a66449699350e1c66e0c602ce0733
Kled <1017yu@daum.net>
2023-05-14 15:13:32 +0900

    docs: GlobalStyle 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import { RecoilRoot } from "recoil";
-import Layout from "~/src/components/Layout";
+import GlobalStyle from "~/src/styles/GlobalStyle";
+import Layout from "../components/Layout";
 
-//  Component에는 각 pages component가 들어오고, pageProps 는 getInitialProps를 통해 들어온 props들을 나타낸다.
+// Component에는 각 pages component가 들어오고,
+// pageProps 는 getInitialProps를 통해 들어온 props들을 나타낸다.
 function MyApp({ Component, pageProps }) {
   // 상태관리를 위한 root에 RecoilRoot wrap
   return (
     <RecoilRoot>
+      <GlobalStyle />
       <Layout>
         <Component {...pageProps} />
       </Layout>
