ea83c8e53d986dcaaaf0cb69934fc2975825b989
Kled <1017yu@daum.net>
2023-05-16 21:11:16 +0900

    feat: Todo-item 배열 길이에 따른 order 번호 부여

--- src/components/TodoItemCreator.jsx
+++ src/components/TodoItemCreator.jsx
@@ -1,7 +1,7 @@
 // Todo-Item을 추가할 수 있도록 input과 button으로 구성
 // createTodo(inputValue)를 통해, todoListState atom 값을 업데이트하여 새로운 Todo-Item을 추가
 import React, { useState } from "react";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { todoListState } from "~/src/states/todoAtoms";
 import { createTodo } from "~/src/lib";
 
@@ -11,13 +11,17 @@ function TodoItemCreator({ onClose }) {
 
   // todoListState atom을 setTodoList 변수에 할당, List 추가 시 atom(recoil)값을 업데이트하여 리렌더링
   const setTodoList = useSetRecoilState(todoListState);
+  const [todoList] = useRecoilState(todoListState);
 
   const handleChange = (e) => setInputValue(e.target.value);
 
   const handleKeyPress = async (e) => {
     if (e.key === "Enter") {
-      const newTodo = await createTodo(inputValue);
-      setTodoList((oldTodoList) => [newTodo, ...oldTodoList]);
+      const newTodo = await createTodo({
+        title: inputValue,
+        order: todoList.length,
+      });
+      setTodoList((oldTodoList) => [...oldTodoList, newTodo]);
 
       // Item add 후 input value 초기화
       setInputValue("");
@@ -28,9 +32,12 @@ function TodoItemCreator({ onClose }) {
   };
 
   const addItem = async () => {
+    const newTodo = await createTodo({
+      title: inputValue,
+      order: todoList.length,
+    });
+    setTodoList((oldTodoList) => [...oldTodoList, newTodo]);
     setInputValue("");
-    const newTodo = await createTodo(inputValue);
-    setTodoList((oldTodoList) => [newTodo, ...oldTodoList]);
 
     onClose();
   };
