fafe6c958194eb601dba285a886960626d1066b1
Kled <1017yu@daum.net>
2023-05-19 21:30:59 +0900

    fix: 500 에러 수정

--- src/components/Todo/TodoItemCreator.jsx
+++ src/components/Todo/TodoItemCreator.jsx
@@ -1,23 +1,28 @@
 // Todo-Item을 추가할 수 있도록 input과 button으로 구성
 // createTodo(inputValue)를 통해, todoListState atom 값을 업데이트하여 새로운 Todo-Item을 추가
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import colors from "../../styles/colors";
 import { BsFillCameraFill } from "react-icons/Bs";
 import { IoIosAppstore } from "react-icons/Io";
-import TodoInput from "~/src/components/Todo/TodoInput";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { todoListState } from "~/src/states/todoAtoms";
+import { FaArrowCircleUp } from "react-icons/Fa";
+import { createTodo } from "~/src/lib";
 
 function TodoItemCreator() {
+  const setTodoList = useSetRecoilState(todoListState);
   const [todoList] = useRecoilState(todoListState);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (e) => setInputValue(e.target.value);
 
   const addItem = async () => {
     const newTodo = await createTodo({
       title: inputValue,
       order: todoList.length,
     });
-    updateTodoList(newTodo);
+    setTodoList([...todoList, newTodo]);
     setInputValue("");
   };
 
@@ -32,9 +37,17 @@ function TodoItemCreator() {
       <Div>
         <BsFillCameraFill className="camera-Icon gray-Icon" />
         <IoIosAppstore className="appStore-Icon gray-Icon" />
-        <TodoInput
-          method={() => addItem(inputValue)}
-          keyPress={handleKeyPress}
+        <Input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          onKeyPress={handleKeyPress}
+          placeholder="할일 입력"
+        />
+        <FaArrowCircleUp
+          className="submitButton-Icon"
+          type="submit"
+          onClick={addItem}
         />
       </Div>
     </>
@@ -61,6 +74,12 @@ const Div = styled.div`
     color: ${colors.blue[5]};
     right: 0.3rem;
   }
+`;
+const Input = styled.input`
+  border-radius: 10px;
+  border-color: ${colors.gray[0]};
+  height: 40px;
+  width: 22rem;
 `;
 
 export default TodoItemCreator;
