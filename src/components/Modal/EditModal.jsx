0044d0b524dd7ce02bd6a8b7aae4581b9a6a0b1a
Kled <1017yu@daum.net>
2023-05-19 15:56:40 +0900

    feat: todo 수정 후 GET으로 리스트 호출

--- src/components/Modal/EditModal.jsx
+++ src/components/Modal/EditModal.jsx
@@ -1,15 +1,16 @@
 // BiEdit Icon 클릭 시, Edit Modal이 open, input의 value 값으로
 // todoListState를 업데이트하여 todoItem을 Edit하는 컴포넌트
-
 import React from "react";
-import { useState } from "react";
-import { useSetRecoilState } from "recoil";
-import { todoListState } from "~/src/states/todoAtoms";
 import styled from "styled-components";
+import { useState } from "react";
+import { useSetRecoilState, useRecoilState } from "recoil";
+import { todoListState } from "~/src/states/todoAtoms";
+import { formattedDate } from "~/src/lib/dateUtils";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import { BiEdit } from "react-icons/Bi";
-import { editTodo } from "~/src/lib";
+import { editTodo, getTodo } from "~/src/lib";
+import TodoInput from "../Todo/TodoInput";
 // import colors from "~/src/styles/colors";
 
 const EditModal = ({ item }) => {
@@ -17,8 +18,8 @@ const EditModal = ({ item }) => {
   const [inputValue, setInputValue] = useState("");
 
   // todoListState atom을 setTodoList 변수에 할당, 추가 시 todoListState(atom) 값을 업데이트하여 리렌더링
+  const [todoList] = useRecoilState(todoListState);
   const setTodoList = useSetRecoilState(todoListState);
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -31,11 +32,9 @@ const EditModal = ({ item }) => {
     order: item.order,
   };
 
-  const handleChange = (e) => setInputValue(e.target.value);
-
   const handleKeyPress = async (e) => {
     if (e.key === "Enter") {
-      // todo 업데이트
+      // Edit 후 onClose를 호출하여 atom 업데이트
       const editedTodo = await editTodo(todo);
 
       // useSetRecoilState로 가져온 todoListState(atom)값 업데이트
@@ -47,6 +46,8 @@ const EditModal = ({ item }) => {
       );
       // Item add 후 input value 초기화
       setInputValue("");
+      updateTodoList(editedTodo);
+
       // Modal Close
       handleClose();
     }
@@ -65,6 +66,23 @@ const EditModal = ({ item }) => {
     handleClose();
   };
 
+  const updateTodoList = async (newTodo) => {
+    const todoListData = await getTodo();
+    const sortedTodoList = todoListData.map((todoItem, index) => {
+      if (todoItem.id === newTodo.id) {
+        return {
+          ...todoItem,
+          order: todoList.length,
+        };
+      }
+      return {
+        ...todoItem,
+        order: index,
+      };
+    });
+    setTodoList(sortedTodoList);
+  };
+
   return (
     <>
       {/* 아이콘 클릭 시, Modal Open */}
@@ -76,13 +94,13 @@ const EditModal = ({ item }) => {
         aria-describedby="modal-modal-description"
       >
         <StyledBox>
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleChange}
-            onKeyPress={handleKeyPress}
-          />
-          <button onClick={updateItem}>수정</button>
+          <StyledDiv>
+            <TodoInput method={updateItem} keyPress={handleKeyPress}>
+              수정
+            </TodoInput>
+          </StyledDiv>
+          <span>생성일: {formattedDate(item.createdAt)}</span>
+          <span>수정일: {formattedDate(item.updatedAt)}</span>
         </StyledBox>
       </Modal>
     </>
@@ -90,6 +108,8 @@ const EditModal = ({ item }) => {
 };
 
 const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -99,6 +119,10 @@ const StyledBox = styled(Box)`
   border: 2px solid #000;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
   padding: 20px;
+`;
+
+const StyledDiv = styled.div`
+  display: block;
 `;
 
 export default EditModal;
