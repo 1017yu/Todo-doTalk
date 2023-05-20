4d56ca0cf1d8c2e4f94f606d1301e71e0e6b794c
Kled <1017yu@daum.net>
2023-05-20 13:32:40 +0900

    docs: Page home 컴포넌트 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
-import TodoList from "../components/TodoList";
+import Header from "~/src/components/Header";
+import TodoList from "~/src/components/Main/TodoList";
+import Footer from "~/src/components/Footer";
 
 function Home() {
   return (
     <>
+      <Header />
       <TodoList />
+      <Footer />
     </>
   );
 }
