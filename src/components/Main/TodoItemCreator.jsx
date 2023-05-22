be9cebe586ef351c4617345af100a04e9406aadd
Kled <1017yu@daum.net>
2023-05-22 14:10:39 +0900

    docs: 코드 별 주석 수정 및 추가

--- src/components/Main/TodoItemCreator.jsx
+++ src/components/Main/TodoItemCreator.jsx
@@ -1,10 +1,9 @@
 // Todo-Item을 추가할 수 있도록 input과 button으로 구성
-// createTodo(inputValue)를 통해, todoListState atom 값을 업데이트하여 새로운 Todo-Item을 추가
+// 입력값을 바탕으로 새로운 Todo-Item을 생성하여 todoListState atom 값을 업데이트
 import React, { useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import colors from "../../styles/colors";
-
-import { useRecoilState, useSetRecoilState } from "recoil";
 import { todoListState } from "~/src/states/todoAtoms";
 import { FaArrowCircleUp } from "react-icons/Fa";
 import { createTodo, getTodo } from "~/src/lib";
@@ -14,15 +13,23 @@ function TodoItemCreator() {
   const [todoList] = useRecoilState(todoListState);
   const [inputValue, setInputValue] = useState("");
 
+  // input의 변화를 inputValue state에 저장
   const handleChange = (e) => setInputValue(e.target.value);
 
+  // Todo-Item 생성 및 atom 업데이트 함수 (POST)
   const addItem = async () => {
+    // 빈 값을 입력하였을 때, alert
+    if (!inputValue.trim()) return window.alert("⌨️ 할 일을 입력하세요!");
+    // POST 요청 데이터 (title: 입력값, order: 현재 atom 내 배열의 길이)
     const newTodo = await createTodo({
       title: inputValue,
       order: todoList.length,
     });
     setTodoList([...todoList, newTodo]);
+
+    // 입력 창 초기화
     setInputValue("");
+    // GET 통신
     getTodo();
   };
 
@@ -74,7 +81,7 @@ const Div = styled.div`
   }
 `;
 const Input = styled.input`
-  border-radius: 10px;
+  border-radius: 1rem;
   border-color: ${colors.gray[0]};
   height: 40px;
   width: 22rem;
